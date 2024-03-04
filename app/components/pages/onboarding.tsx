d58068d9a26f39b453387b671fd0401f2a400803
Liam <n11102764@qut.edu.au>
2024-03-04 17:51:23 +1000

    UI: Began onboarding page.

---
+++ app/components/pages/onboarding.tsx
@@ -0,0 +1,7 @@
+export default function Onboarding() {
+    return (
+        <main className="xl:px-[20vw] lg:px-[15vw] md:px-[5vw] px-[2vw] min-h-screen text-text mt-8">
+            <h1 className="text-4xl font-semibold text-center">Master your Knowledge with FlashCards</h1>
+        </main>
+    )
+}
